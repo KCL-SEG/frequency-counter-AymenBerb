14de12c601ca7513f4e9e8e3f9f23ace2c971c53
Aymen Berbache <aymenberbache21@gmail.com>
2022-09-27 14:34:17 +0100

    finished

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    itemsCount = [str(i) for i in items]
+    keys = list(set(items))
+    for key in keys:
+        frequencies[str(key)] = itemsCount.count(str(key))
     return frequencies
+
+#print(frequencies([100, 'Hello', '100', '100', 100]))
+
+
